c412df6bec991370592d9688d58c01f6fc92c937
Gustavo <gu.poiatti.consorte@gmail.com>
2026-09-06 00:21:52 -0300

    Aponta a pagina para o tunel de agora

--- config.js
+++ config.js
@@ -12,4 +12,4 @@
  *
  * Sem barra no fim.
  */
-window.HEROTRADE_API = "https://usgs-sophisticated-life-felt.trycloudflare.com";
+window.HEROTRADE_API = "https://john-homeless-oct-gene.trycloudflare.com";
