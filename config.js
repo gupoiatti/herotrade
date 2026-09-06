574a745aa72ecfd11195647507d143f7853f387e
Gustavo <gu.poiatti.consorte@gmail.com>
2026-09-05 21:40:31 -0300

    Aponta a pagina para o tunel de agora

--- config.js
+++ config.js
@@ -12,4 +12,4 @@
  *
  * Sem barra no fim.
  */
-window.HEROTRADE_API = "https://essays-chain-acknowledged-awarded.trycloudflare.com";
+window.HEROTRADE_API = "https://reliable-jpeg-mix-unavailable.trycloudflare.com";
